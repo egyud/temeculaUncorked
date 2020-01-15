a31d4ac3169c3c8e4e1e2318ac5ae68f1a8b5fca
Andy <andy.egyud@gmail.com>
2020-01-15 10:01:55 -0800

    Initial setup for EventsPage

---
+++ pages/EventsPage/EventsPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import EventList from '../../components/EventList'
+
+export default EventsPage = () => {
+  return (
+    <View>
+      <View>
+        <ImageBackground
+          source={require('../../assets/wineGlasses.jpg')}
+          style={styles.imageBackground}
+        >
+          <Text style={styles.headline}>Upcoming Events</Text>
+        </ImageBackground>
+      </View>
+      <EventList />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    width: '100%', 
+    paddingVertical: 40,
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headline: {
+    fontSize: 30,
+    color: '#89012c',
+    backgroundColor: '#99ff99',
+    paddingVertical: 5,
+    paddingHorizontal: 10
+  }
+})
