4b21ba7ebbe452dad6a61b0c7735500843f94622
Andy <andy.egyud@gmail.com>
2020-02-05 15:32:13 -0800

    Added more setup for activityfeed

--- client/components/ActivityFeed.js
+++ client/components/ActivityFeed.js
@@ -1,70 +1,52 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import { List, ListItem, Left, Right, Body, Thumbnail, Text } from 'native-base'
 
-export default ReviewList = () => {
+export default ActivityFeed = ({ navigation }) => {
+  const [reviews, updateReviews] = useState([])
+
+  useEffect(() => {
+    getReviews()
+  }, [])
+
+  function getReviews() {
+    axios.get('http://localhost:5000/api/reviews/winery/recent')
+      .then(res => updateReviews(res.data.reviews))
+      .catch(err => console.error(err))
+  }
+
+  function modifyTimestamp(timestamp) {
+    let newTime = timestamp
+      .slice(0, 10)
+      .split('-')
+      .map(num => Number(num))
+    // need to remove one from the month to get right date, as moment starts from index 0
+    newTime[1]--
+    return newTime
+  }
+
   return (
     <List>
-      <ListItem avatar style={styles.listItem}>
-        <Left>
-          <Thumbnail/>
-        </Left>
-        <Body>
-          <Text>Samwise B.</Text>
-          <Text note>Reviewed Doffo Winery</Text>
-        </Body>
-        <Right>
-          <Text note>3:43pm</Text>
-        </Right>
-      </ListItem>
-      <ListItem avatar style={styles.listItem}>
-        <Left>
-          <Thumbnail/>
-        </Left>
-        <Body>
-          <Text>Amy R</Text>
-          <Text note>Reviewed Leoness Cellars</Text>
-        </Body>
-        <Right>
-          <Text note>2:22pm</Text>
-        </Right>
-      </ListItem>
-      <ListItem avatar style={styles.listItem}>
-        <Left>
-          <Thumbnail source={{ uri: '../../assets/IMG_0684 copy.JPG' }}/>
-        </Left>
-        <Body>
-          <Text>Lester</Text>
-          <Text note>Reviewed Wiens Family Cellars</Text>
-        </Body>
-        <Right>
-          <Text note>12:30pm</Text>
-        </Right>
-      </ListItem>
-      <ListItem avatar style={styles.listItem}>
-        <Left>
-          <Thumbnail/>
-        </Left>
-        <Body>
-          <Text>Andy E</Text>
-          <Text note>Reviewed Akash Winery</Text>
-        </Body>
-        <Right>
-          <Text note>12:28pm</Text>
-        </Right>
-      </ListItem>
-      <ListItem avatar style={styles.listItem}>
-        <Left>
-          <Thumbnail/>
-        </Left>
-        <Body>
-          <Text>Joe G.</Text>
-          <Text note>Reviewed Lorimar Something</Text>
-        </Body>
-        <Right>
-          <Text note>Yesterday</Text>
-        </Right>
-      </ListItem>
+      {reviews.map(review => (
+        <TouchableOpacity 
+          onPress={() => navigation.navigate('Review', { review })}
+          key={review._id}>
+          <ListItem avatar style={styles.listItem}>
+            <Left>
+              <Thumbnail source={{ uri: review.userId.avatar.url }}/>
+            </Left>
+            <Body>
+              <Text>{review.userId.name}</Text>
+              <Text note>Reviewed {review.reviewedId.name}</Text>
+            </Body>
+            <Right>
+              <Text note>{moment(modifyTimestamp(review.timestamp)).fromNow()}</Text>
+            </Right>
+          </ListItem>
+        </TouchableOpacity>
+      ))}
     </List>
   )
 }
