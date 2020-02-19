acc5d91c29f293a5516acfc69d117bb5a83bcce6
Andy <andy.egyud@gmail.com>
2020-02-19 13:39:23 -0800

    Added header image and link to comparison screen

--- client/pages/ClubListScreen.js
+++ client/pages/ClubListScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchWineClubs } from '../actions/wineActions'
-import { View, StyleSheet, Text, Modal } from 'react-native'
+import { View, StyleSheet, ImageBackground, Text, Modal } from 'react-native'
 import { Form, Picker, Button } from 'native-base'
 import ClubList from '../components/ClubList'
 
@@ -34,6 +34,19 @@ const ClubListScreen = ({ wineClubs, fetchWineClubs, navigation }) => {
 
   return (
     <View style={styles.container}>
+      <View>
+        <ImageBackground
+          source={require('../assets/wineGlasses.jpg')}
+          style={styles.imageBackground}
+        >
+          <Text style={styles.headline}>Wine Clubs</Text>
+        </ImageBackground>
+        <View>
+          <Text
+            onPress={() => navigation.navigate('Comparison')} 
+            style={styles.compareLink}>Click here to compare wine club benefits</Text>
+        </View>
+      </View>
       <ClubList 
         clubs={clubsList}
         navigation={navigation}/>
@@ -52,6 +65,28 @@ ClubListScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  imageBackground: {
+    width: '100%', 
+    paddingVertical: 40,
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headline: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: '#fff',
+    backgroundColor: '#89012c',
+    paddingVertical: 25,
+    paddingHorizontal: 40
+  },
+  compareLink: {
+    textAlign: 'center',
+    backgroundColor: '#99ff99',
+    paddingVertical: 15,
+    color: '#614d36',
+    fontWeight: '500'
   }
 })
 
