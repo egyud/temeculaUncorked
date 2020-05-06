f9fa5ad4086755c0c144d137e2382712758a4feb
Andy <andy.egyud@gmail.com>
2020-05-06 13:02:03 -0700

    Added wineryList dropdown to each form, and removed forms meant for updating

--- management/management.controller.js
+++ management/management.controller.js
@@ -1,6 +1,7 @@
 const { getWines } = require('../utils/getWines') 
 const { getClubs } = require('../utils/getClubs') 
 const { getEvents } = require('../utils/getEvents') 
+const wineryList = require('../data/wineryList')
 
 exports.getLoginPage = (req, res) => {
   res.render('login')
@@ -11,44 +12,19 @@ exports.getIndexPage = (req, res) => {
 }
 
 exports.getAddEventForm = (req, res) => {
-  res.render('addEvent')
-}
-
-exports.getUpdateEventForm = (req, res) => {
-  res.render('updateEvent')
+  res.render('addEvent', {
+    wineryList
+  })
 }
 
 exports.getAddWineForm = (req, res) => {
-  res.render('addWine')
-}
-
-exports.getUpdateWineForm = (req, res) => {
-  res.render('updateWine')
+  res.render('addWine', {
+    wineryList
+  })
 }
 
 exports.getAddClubForm = (req, res) => {
-  res.render('addClub')
-}
-
-exports.getUpdateClubForm = (req, res) => {
-  res.render('updateClub')
-}
-
-exports.getUpdateInfoForm = (req, res) => {
-  res.render('updateInfo')
-}
-
-exports.getClubListPage = async (req, res) => {
-  let clubs = await getClubs()
-  res.render('clubList')
-}
-
-exports.getEventListPage = async (req, res) => {
-  let events = await getEvents()
-  res.render('eventList')
-}
-
-exports.getWineListPage = async (req, res) => {
-  let wines = await getWines()
-  res.render('wineList')
+  res.render('addClub', {
+    wineryList
+  })
 }
--- management/management.routes.js
+++ management/management.routes.js
@@ -12,28 +12,10 @@ router.get('/index', ManagementController.getIndexPage)
 // add event form
 router.get('/addEvent', ManagementController.getAddEventForm)
 
-// update/delete event form
-router.get('/updateEvent', ManagementController.getUpdateEventForm)
-
 // add wine form
 router.get('/addWine', ManagementController.getAddWineForm)
 
-// update/delete wine form
-router.get('/updateWine', ManagementController.getUpdateWineForm)
-
 // add club form
 router.get('/addClub', ManagementController.getAddClubForm)
-
-// update/delete club form
-router.get('/updateClub', ManagementController.getUpdateClubForm)
-
-// update winery info form
-router.get('/updateInfo', ManagementController.getUpdateInfoForm)
-
-router.get('/eventList', ManagementController.getEventListPage)
-
-router.get('/clubList', ManagementController.getClubListPage)
-
-router.get('/wineList', ManagementController.getWineListPage)
 
 module.exports = router
