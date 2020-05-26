d726a0ce36a627eaf605c04384e79108af8a0635
Andy <andy.egyud@gmail.com>
2020-05-26 11:06:04 -0700

    added dotenv

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,8 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
-const mongoose = require('mongoose')
 const User = require('../users/user.model')
+
+require('dotenv').config()
 
 const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
